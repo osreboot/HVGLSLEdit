ebe5306b655283c2dc4bcd38a9bff9976586eee1
osreboot <calvin.16.weaver@gmail.com>
2015-09-15 18:51:05 -0700

    Added NodeSingleFloatToColor

---
+++ src/com/osreboot/glsledit/node/NodeSingleFloatToColor.java
@@ -0,0 +1,39 @@
+package com.osreboot.glsledit.node;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.osreboot.glsledit.Node;
+import com.osreboot.glsledit.Pin;
+import com.osreboot.glsledit.pin.PinColor;
+import com.osreboot.glsledit.pin.PinFloat;
+
+public class NodeSingleFloatToColor extends Node{
+
+	private PinFloat input;
+	private PinColor output;
+	
+	public NodeSingleFloatToColor(float x, float y){
+		super("single float to color", x, y, Node.COLOR_CAST);
+		input = new PinFloat(this, "in");
+		setInputs(new ArrayList<Pin>(Arrays.asList(input)));
+		output = new PinColor(this, "out"){
+			@Override
+			public String getOutput(){
+				return getContent().get(0);
+			}
+		};
+		setOutputs(new ArrayList<Pin>(Arrays.asList(output)));
+	}
+
+	@Override
+	public ArrayList<String> getContent(){
+		return new ArrayList<String>(Arrays.asList("vec4(" + Pin.getConnectionOutput(input, "0") + "," + Pin.getConnectionOutput(input, "0") + "," + Pin.getConnectionOutput(input, "0") + "," + Pin.getConnectionOutput(input, "1") + ")"));
+	}
+
+	@Override
+	public Node getNext(){
+		return null;
+	}
+
+}
